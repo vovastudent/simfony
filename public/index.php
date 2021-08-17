6f7cad54386795e048fe28adbae86cb0f86c6dd2
vovastudent <vzin1986@gmail.com>
2021-08-18 01:13:59 +0300

    unit_04

--- public/index.php
+++ public/index.php
@@ -2,6 +2,40 @@
 
 use App\Kernel;
 
+//class MyClass
+//{
+//       public $name = "My name";
+//
+//       protected $sex = 'mail';
+//       private $id;
+//
+//    public function __construct($name, $sex, $id)
+//    {
+//        $this->name = $name;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getSex()
+//    {
+//        return $this->sex;
+//    }
+//
+//    /**
+//     * @param mixed $sex
+//     */
+//    public function setSex($sex): void
+//    {
+//        $this->sex = $sex;
+//    }
+//
+//
+//
+//};
+//$instance =  new MyClass();
+//$instance->getSex( sex: 'femail');
+//var_dump($instance->getSex());
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
--- src/Controller/MyClass.php
+++ src/Controller/MyClass.php
@@ -2,7 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class MyClass
 {
+
+    public function __construct()
+    {
+        echo "I'm constructor";
+    }
+
+    public function index(): Response
+    {
+        return new Response("<h1>Hello from controller</h1>");
+
+    }
 
 }
